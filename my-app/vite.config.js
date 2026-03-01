28e1994ea07355a1bfc043d08a7733c1e8173bc6
Benson Khau <bensonkhau@gmail.com>
2026-02-28 22:45:16 -0800

    Migrate from Create React App to Vite

---
+++ my-app/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react({
+    jsxRuntime: 'automatic'
+  })],
+  base: '/planner/',
+  build: {
+    outDir: 'build'
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: []
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
+  }
+})
